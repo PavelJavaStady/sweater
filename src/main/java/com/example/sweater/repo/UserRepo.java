cafc5129ed589d1252b317bea0b8f30cf77fabf0
PavelJavaStady <PashaJava@yandex.by>
2022-07-12 15:56:28 +0300

    Add Mail

---
+++ src/main/java/com/example/sweater/repo/UserRepo.java
@@ -0,0 +1,12 @@
+package com.example.sweater.repo;
+
+import com.example.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+
+public interface UserRepo extends JpaRepository<User, Long> {
+    User findByUsername(String username);
+
+    User findByActivationCode(String code);
+}
